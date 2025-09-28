12ca25741b6d124b097c5fbdd9a29db1d2b00125
VhanganiMugeri17 <162291365+VhanganiMugeri@users.noreply.github.com>
2025-09-28 19:23:49 +0200

    Update introVar.py

--- introVar.py
+++ introVar.py
@@ -1,1 +1,17 @@
+first_name = "Vhangani"
+last_name="Mugeri"
+full_name= first_name + last_name
+print("hello "+full_name+" hope you doing well bro")
+
+
+int= 22
+int +=1
+print((int))
+
+height=25.55
+print("your height is: "+str(height))
+
+human= True
+print("are you a human : "+str(human))
+
 
